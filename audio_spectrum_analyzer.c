fc1ddbbe620879a92303d9aec4e9aa0b20d54710
harka069 <kristof.frelih@gmail.com>
2025-01-08 23:54:58 +0100

    cleaning, fixed a potencial bug on line 31

--- audio_spectrum_analyzer.c
+++ audio_spectrum_analyzer.c
@@ -30,7 +30,7 @@
 
 int16_t sample_buf[N_SAMPLES];
 
-int8_t display_bars[31];
+int8_t display_bars[32];
 
 void __not_in_flash_func(adc_capture)(uint16_t *buf, size_t count) {
     adc_fifo_setup(true, false, 0, false, false);
@@ -65,69 +65,51 @@ int main()
 
     arm_status status;
     
-    
-
-  
     while (true) 
     {
+      
+        printf("\nStarting capture\n");
+        uint64_t start_adc_conversion = time_us_64();
+        adc_capture(sample_buf, N_SAMPLES);
+        uint64_t stop_adc_conversion = time_us_64();
+        uint32_t time_difference = stop_adc_conversion - start_adc_conversion;
+        printf("ADC conversion time: %u microseconds\n", time_difference);
+        
 
-        /*random values on display*/
-        /*for (int j = 0;j < 33; j++)            {   
-                uint32_t random = get_rand_32();
-                uint8_t percent = (uint8_t)((random / (float)0xFFFFFFFF) * 100);
-                GFX_soundbar(j*10,240,9,240,ILI9341_BLUE,ILI9341_RED,percent);               
-            }
-        GFX_flush();  */ 
-        /*char c = getchar();
-        printf("%c", c);
+        /************************************************************************************************************
+        *   Internally input is downscaled by 2 for every stage to avoid saturations inside CFFT/CIFFT process.     *
+        *   Hence the output format is different for different RFFT sizes.                                          *        
+        *   The input and output formats for different RFFT sizes and number of                                     *
+        *   bits to upscale are mentioned in the tables below for RFFT and RIFFT:                                   *
+        *    https://arm-software.github.io/CMSIS_5/DSP/html/group__RealFFT.html#ga00e615f5db21736ad5b27fb6146f3fc5 *                      
+        *   RFFT Size   Input Format   Output Format Number of Bits to upscale
+        *   32              
+        *   64
+        *   128
+        *********************************************************************************************************/
 
-        switch (c) {
-            case 's': {
-                uint32_t result = adc_read();
-                const float conversion_factor = 3.3f / (1 << 12);
-                printf("\n0x%03x -> %f V\n", result, result * conversion_factor);
-                break;
+        status = arm_rfft_init_q15(&fft_instance, 256 /*bin count*/, 0 /*forward FFT*/, 1 /*output bit order is normal*/);
+        arm_rfft_q15(&fft_instance, (q15_t*)sample_buf, output);
+        arm_abs_q15(output, output, FFT_SIZE);
+        for(uint16_t i = 0; i < 511;i++)
+        {
+            output_int[i]=output[i];
+        }
+        for (uint8_t i = 0; i <= 31; i++) // Calculate bars on display -> 256 samples into 32 bars
+        {
+            uint16_t sum = 0;
+            for (uint8_t j = 0; j < 4; j++) {
+                sum =sum+ output_int[i * 4 + j];
             }
-            case 'S': {*/
-                printf("\nStarting capture\n");
-                uint64_t start_adc_conversion = time_us_64();
-                adc_capture(sample_buf, N_SAMPLES);
-                uint64_t stop_adc_conversion = time_us_64();
-                uint32_t time_difference = stop_adc_conversion - start_adc_conversion;
-                printf("ADC conversion time: %u microseconds\n", time_difference);
-                status = arm_rfft_init_q15(&fft_instance, 256 /*bin count*/, 0 /*forward FFT*/, 1 /*output bit order is normal*/);
-                arm_rfft_q15(&fft_instance, (q15_t*)sample_buf, output);
-                arm_abs_q15(output, output, FFT_SIZE);
-                for(uint16_t i = 0; i < 511;i++)
-                {
-                    output_int[i]=output[i];
-                }
-                for (uint8_t i = 0; i <= 31; i++) 
-                {
-                    uint16_t sum = 0;
-                    for (uint8_t j = 0; j < 4; j++) {
-                        sum =sum+ output_int[i * 4 + j];
-                    }
-                    sum=sum/4;
-                    display_bars[i] = (uint8_t)sum;  // Calculate average
-                }
-                for (int j = 0;j <= 31; j++)
-                {   
-                uint8_t percent = (uint8_t)((display_bars[j] * 100*4/*4 samo skalira*/)/256);
-                GFX_soundbar(j*10,240,9,240,ILI9341_BLUE,ILI9341_RED,percent);              
-                }
-                GFX_flush();  
-                /*break;
-            }
-            
-            case '\n':
-            case '\r':
-                break;
-            default:
-                printf("\nUnrecognised command: %c\n", c);
-                break;
-        }*/
-             
+            sum=sum/4;
+            display_bars[i] = (uint8_t)sum;  
+        }
+        for (int j = 0;j <= 31; j++)
+        {   
+            uint8_t percent = (uint8_t)((display_bars[j] * 100*4)/256); /*4 scaling?*/
+            GFX_soundbar(j*10,240,9,240,ILI9341_BLUE,ILI9341_RED,percent);              
+        }
+        GFX_flush();               
     }
 }
 void InitializeDisplay(uint16_t color)
